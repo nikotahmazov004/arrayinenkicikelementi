075b225c852cae400e80275bd3ccc30b59bcf3ec
Nicat Tahmazov <nijattahmazovv@gmail.com>
2022-08-03 18:20:05 +0400

    task

---
+++ ArrayinEnKicikElementiniTap1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ArrayinEnKicikElementiniTap1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 8, 16, 27, 4, 35 };
+            int min = 16;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]<min)
+                {
+                    min = arr[i];
+                }
+            }
+            Console.WriteLine(min);
+        }
+    }
+}
